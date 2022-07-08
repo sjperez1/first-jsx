7bca751e4b11470394c949c61a2698ebec4d64ad
sjperez1 <sarahpz479@gmail.com>
2022-07-08 17:16:28 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import List from './components/List';
 import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Hello Dojo!</h1>
-        <h3>Things I need to do:</h3>
-        <li>Learn React</li>
-        <li>Climb Mt. Everest</li>
-        <li>Run a marathon</li>
-        <li>Feed the dogs</li>
+        <List/>
     </div>
   );
 }
